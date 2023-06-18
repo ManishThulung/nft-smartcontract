a691a78c4e1c25ab6a6b02b4f485a62d2ca8535d
ManishThulung <manish.thulung58@gmail.com>
2023-06-18 18:01:52 +0545

    initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,66 @@
+require("@nomiclabs/hardhat-waffle")
+require("@nomiclabs/hardhat-etherscan")
+require("hardhat-deploy")
+require("solidity-coverage")
+require("hardhat-gas-reporter")
+require("hardhat-contract-sizer")
+require("dotenv").config()
+
+/** @type import('hardhat/config').HardhatUserConfig */
+
+const GOERLY_RPC_URL = process.env.GOERLY_RPC_URL
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+const COINMARKET_API_KRY = process.env.COINMARKET_API_KRY
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      blockConfirmations: 1,
+    },
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: PRIVATE_KEY !== undefined ? [PRIVATE_KEY] : [],
+      //   accounts: {
+      //     mnemonic: MNEMONIC,
+      //   },
+      blockConfirmations: 6,
+      saveDeployments: true,
+      chainId: 11155111,
+    },
+    goerli: {
+      chainId: 5,
+      blockConfirmations: 6,
+      url: GOERLY_RPC_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      sepolia: ETHERSCAN_API_KEY,
+    },
+  },
+  solidity: "0.8.7",
+  gasReporter: {
+    enabled: false,
+    outputFile: "gas-report.txt",
+    noColors: true,
+    currency: "USD",
+    coinmarketcap: COINMARKET_API_KRY,
+    token: "MATIC",
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0,
+    },
+    player: {
+      default: 1,
+    },
+  },
+  mocha: {
+    timeout: 300000, //300sec
+  },
+}
